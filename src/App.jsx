56f82443b9541f585d42428ae244ee2f624b5061
Uche <uchaynwankwo@gmail.com>
2022-12-21 18:16:29 +0100

    implement ticking gradient

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2 className='texts'>The Clock is ticking.</h2>
+    </div>
+  )
+}
+
+export default App
